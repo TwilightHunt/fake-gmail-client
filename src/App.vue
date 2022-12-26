82b11733ac2b09462a292f12bb83e6d4e80c0045
Vera Lavrentyeva <55912590+TwilightHunt@users.noreply.github.com>
2022-12-26 15:15:48 +0300

    fix: font settings

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,23 @@
 @font-face {
   font-family: "Google Sans";
   src: url("./fonts/GoogleSans-Regular.ttf") format("truetype");
+  font-weight: 400;
+}
+@font-face {
+  font-family: "Google Sans";
+  src: url("./fonts/GoogleSans-Medium.ttf") format("truetype");
+  font-weight: 600;
+}
+@font-face {
+  font-family: "Google Sans";
+  src: url("./fonts/GoogleSans-Bold.ttf") format("truetype");
+  font-weight: 800;
 }
 * {
   font-family: "Google Sans";
-  font-weight: 700;
+  font-weight: 600;
+  font-size: 0.875rem;
+  line-height: 114%;
 }
 body {
   overflow: hidden;
