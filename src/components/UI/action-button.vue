f0f953ff3290725f0c2cc868623bbcdaa8b4f965
Vera Lavrentyeva <55912590+TwilightHunt@users.noreply.github.com>
2023-01-04 17:52:01 +0300

    build: basics styles

---
+++ src/components/UI/action-button.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-btn icon elevation="0" class="action-btn">
+      <icon :name="name" :size="size" class="icon" color="#333333" />
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    size: Number,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/compose.vue
+++ src/components/compose.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="compose">compose</div>
+  <div class="compose">
+    <header class="header">
+      <div class="title">New message</div>
+      <div class="tools">
+        <div class="tool">
+          <v-icon color="#202124" class="icon" size="16">
+            mdi-window-minimize
+          </v-icon>
+          <v-icon color="#202124" class="icon" size="16">
+            mdi-arrow-expand
+          </v-icon>
+          <v-icon color="#202124" class="icon" size="16">
+            mdi-close-thick
+          </v-icon>
+        </div>
+      </div>
+    </header>
+    <form action="" class="form">
+      <input type="text" class="receiver" placeholder="Addressees" />
+      <input type="text" class="topic" placeholder="Topic" />
+      <textarea name="" class="message"></textarea>
+    </form>
+    <footer class="footer">
+      <div class="send-button">
+        <button class="button-text">Send</button>
+        <button class="button-icon">
+          <v-icon color="#fff"> mdi-menu-down </v-icon>
+        </button>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -12,9 +42,92 @@ export default {};
   position: absolute;
   background-color: #fff;
   width: 32.125vw;
-  height: 61vh;
+  min-height: 61vh;
   right: 4.5rem;
-  bottom: 0;
+  bottom: 3px;
   margin-right: 3px;
+  box-shadow: 0px 8px 10px 1px rgb(0 0 0 / 14%),
+    0px 3px 14px 2px rgb(0 0 0 / 12%), 0px 5px 5px -3px rgb(0 0 0 / 20%);
+  border-radius: 8px 8px 0 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.form {
+  flex: 1 1 auto;
+  margin-bottom: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 0 16px;
+  & input {
+    outline: none;
+    display: block;
+    width: 100%;
+    padding: 10px 0;
+  }
+}
+.header {
+  cursor: pointer;
+  background-color: #eef3fc;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.title {
+  padding: 10px 0;
+  margin-left: 16px;
+}
+.icon {
+  width: 16px;
+  height: 16px;
+  &:hover {
+    background-color: #dadcdf;
+  }
+}
+.tools {
+  padding-right: 12px;
+  & .icon {
+    margin-left: 4px;
+  }
+}
+.receiver,
+.topic {
+  border-bottom: solid 1px #dadcdf;
+}
+.message {
+  resize: none;
+  outline: none;
+  flex: 1 0 100%;
+  padding: 10px 0;
+  font-size: 0.83rem;
+}
+.footer {
+  padding: 12px 16px;
+}
+.send-button {
+  & button {
+    background-color: #0b57d0;
+    color: #fff;
+    height: 36px;
+  }
+  &:hover {
+    & button {
+      background-color: #1b61d1;
+      box-shadow: 1px 1px 2px #1b61d1;
+    }
+  }
+}
+.button-text {
+  padding: 0 16px;
+  font-weight: 600;
+  font-size: 0.875rem;
+  max-width: 104px;
+  min-width: 72px;
+  border-radius: 18px 0px 0 18px;
+}
+.button-icon {
+  border-radius: 0 18px 18px 0;
+  border-left: 1px solid #062e6f;
+  padding: 0 7px;
 }
 </style>
